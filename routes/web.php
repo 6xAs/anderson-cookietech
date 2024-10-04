ed8b7fac407f9f885e114fedaf8ec6494e9a7b63
anderson_seixas <anderson.6xas@gmail.com>
2024-10-04 18:00:39 -0400

    teste para aluno

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+################  ROTAS RECEITAS ############## (ANDERSON😒😒)
+Route::get('/gerenciar-receitas', function () {
+    return view('pages.receita.index');
+});
+
+
+
+################  ROTAS RECEITAS ############## (ANDERSON😒😒)
+
+
+
+################  ROTAS CHEFE ############## (JEHE🤑)
+Route::get('/gerenciar-chefe', function () {
+    return view('pages.chefe.index');
+});
+
+
+
+################  ROTAS CHEFE ############## (JHE🤑)
